c33e4b6b74011ef35f3a54340daaeec674e0fc20
SHILPI <121664453+shilpi-nkk@users.noreply.github.com>
2023-01-02 19:26:01 +0530

    Initial commit

---
+++ src/components/Avatar.js
@@ -0,0 +1,8 @@
+// import React from "./react";
+// function Avatar(_props){
+// return (
+//   <img className="circle-img" src={_props.img} alt="avatar_img" />;
+// )
+// }
+
+// export default Avatar
